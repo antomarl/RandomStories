f8659e68e5f799901d3df0bcd5c00a9a23b33b01
antomarl <antoninomarletta09@gmail.com>
2026-06-18 12:01:34 +0200

    I created three functions: getActiveDifficultyName, which gives you the name of the difficulty. getActiveDifficulty, gives you the complete object, with all the details. setActiveDifficulty, actually changes it.

---
+++ js/stato/difficoltaAttiva.js
@@ -0,0 +1,40 @@
+// questo modulo tiene traccia di quale difficolta sta usando l'utemte in questo momento
+// e la salva in localStorage così resta tra una sessione e l'altra
+
+import { difficolta } from "../config/difficolta.js";
+
+// creo la chiave usata per il localStorage(la metto come costante)
+const chiaveStorage = "randomStories_difficoltà";
+
+// come difficlota di default mettiamo normale,se l'utenet non l'ha mai scelta
+const difficoltaDefault = "normale";
+
+// restituisco il nome della difficoltà attiva
+// se non c'è ne ,allora quella di default
+export function nomeDIfficoltaAttiva() {
+    const salvata = localStorage.getItem(chiaveStorage);
+
+    //controllo che esiste e che sia una difficoltàb valida
+    if (salvata && difficolta[salvata]) {
+        return salvata;
+    }
+
+    return difficoltaDefault;
+}
+// restituisco l'oggetto completo della difficoltà attiva
+export function getDifficoltaAttiva() {
+    const nome = getDifficoltaAttiva();
+    return difficolta[nome];
+}
+
+// questa cambia la difficolta attiva e la salva in local storage
+// restituisce true se è andato bene,false se il nome mnon è validao
+export function  setDifficoltaAttiva(nome) {
+    if (!difficolta[nome]) {
+        console.warn("difficolta NON valida: ", nome);
+        return false;
+    }
+
+    localStorage.setItem(chiaveStorage, nome);
+    return true;
+}
